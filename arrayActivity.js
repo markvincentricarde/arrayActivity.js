2ac62167bb6442d5a25be7203fbaed043d13c3c0
markvincentricarde <vincentricarde@gmail.com>
2024-12-15 14:06:55 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+
+let favSingers = ["Arthur Nery", "James Arthur", "Addie"]; //variable favSingers containing 3 singers
+
+
+console.log(favSingers[0]);  //logs first singer in the array
+
+
+let favNumbers = [3,4,8,10]; //favNumbers with 4 numbers
+
+
+let mixedArr = ["string", ["otherarray"], 123, true]; //array wit mixed data types
+
+//logs each item in mixedArr
+console.log(mixedArr[0]);  
+console.log(mixedArr[1]);  
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);  
